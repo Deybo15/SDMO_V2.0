0febf1fd333235c6a5fcda3022f4e4fdd25b3152
Deybo15 <59756716+Deybo15@users.noreply.github.com>
2025-10-16 16:36:28 -0600

    Add Supabase configuration to config.js

---
+++ public/config.js
@@ -0,0 +1,5 @@
+// public/config.js
+window.SDMO_CONFIG = {
+  SUPABASE_URL: "https://qpccqoeronbcdyejfjod.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwY2Nxb2Vyb25iY2R5ZWpmam9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyMzA5MzIsImV4cCI6MjA1OTgwNjkzMn0.v_KA6VdHl-F3sRiVaFMsfDQCS1qKERFBk5mTBcSiIDQ"
+};
